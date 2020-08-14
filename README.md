1ea9604651d3e40b5a932a590f6323eb402c56e6
Brandon Gulla <brandon@megacarbide.local>
2020-08-14 05:49:19 -0400

    baseline

---
+++ README.md
@@ -0,0 +1,10 @@
+# k3ama - Air-gap Migration Assistant
+
+
+
+### Other possible names
+* k3vac
+* k3ziplock
+* k3wh - k3 wormhole
+* k3cia - Comms insensensitive Assistant
+* k3diode
---
+++ scripts/save-docker-image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+IMAGE_NAME="$1"
+SAVE_DIR="$2"
+
+if [ -z "${IMAGE_NAME}" ]; then
+  echo "[Usage] ./save-docker-image.sh <image_name>"
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  SAVE_DIR="."
+
+fi
+
+echo "Creating ${IMAGE_NAME} backup..."
+docker save ${IMAGE_NAME} | gzip --stdout > ${SAVE_DIR}/${IMAGE_NAME}.tgz
