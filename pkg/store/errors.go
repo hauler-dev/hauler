cd93d7aaeaa08b0c7201b6ee1dfdc351145ede23
Josh Wolf <josh@wolfs.io>
2021-12-09 11:09:09 -0700

    make our implementation of oci content store public, remove redundant wrapper Store methods in favor of OCI implementation, add tests for store.Copy*()

---
+++ pkg/store/errors.go
@@ -0,0 +1,7 @@
+package store
+
+import "github.com/pkg/errors"
+
+var (
+	ErrInvalidReference = errors.New("invalid reference")
+)
--- pkg/store/store_test.go
+++ pkg/store/store_test.go
@@ -5,14 +5,22 @@ import (
 	"context"
 	"encoding/json"
 	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 
+	"github.com/google/go-containerregistry/pkg/name"
 	gv1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/partial"
 	"github.com/google/go-containerregistry/pkg/v1/random"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"oras.land/oras-go/pkg/content"
+	"oras.land/oras-go/pkg/target"
 
+	_ "github.com/distribution/distribution/v3/registry/storage/driver/filesystem"
+	_ "github.com/distribution/distribution/v3/registry/storage/driver/inmemory"
+
+	"github.com/rancherfederal/hauler/internal/server"
 	"github.com/rancherfederal/hauler/pkg/artifact"
 	"github.com/rancherfederal/hauler/pkg/store"
 )
@@ -112,4 +120,90 @@ func genArtifact(t *testing.T, ref string) (artifact.OCI, ocispec.Descriptor) {
 		t.Fatal(err)
 	}
 	return &testImage{Image: img}, m
+}
+
+func TestStore_CopyAll(t *testing.T) {
+	ctx := context.Background()
+
+	tmpdir, err := os.MkdirTemp("", "hauler")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmpdir)
+
+	tmpdirRegistry := filepath.Join(tmpdir, "registry")
+	if err := os.Mkdir(tmpdirRegistry, os.ModePerm); err != nil {
+		t.Fatal()
+	}
+
+	tmpdirStore := filepath.Join(tmpdir, "store")
+	if err := os.Mkdir(tmpdirStore, os.ModePerm); err != nil {
+		t.Fatal()
+	}
+
+	r := server.NewTempRegistry(ctx, tmpdirRegistry)
+	if err := r.Start(); err != nil {
+		t.Error(err)
+	}
+	defer r.Stop()
+
+	rc, err := content.NewRegistry(content.RegistryOptions{Insecure: true})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type args struct {
+		ctx      context.Context
+		to       target.Target
+		toMapper func(string) (string, error)
+		refs     []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "should copy an artifact to a registry",
+			args: args{
+				ctx:      ctx,
+				to:       rc,
+				toMapper: nil,
+				refs:     []string{"tester:tester"},
+			},
+			wantErr: false,
+		},
+		{
+			name: "should copy a lot of artifacts to a registry (test concurrency)",
+			args: args{
+				ctx:      ctx,
+				to:       rc,
+				toMapper: nil,
+				refs:     []string{"a/b:c", "a/c:b", "b/c:d", "z/y:w", "u/q:a", "y/y:y", "z/z:z", "b/b:b", "c/c:c"},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := store.NewStore(tmpdirStore)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			for _, ref := range tt.args.refs {
+				locRef, _ := name.ParseReference(ref, name.WithDefaultRegistry(r.Registry()))
+				a, _ := genArtifact(t, locRef.Name())
+				if _, err := s.AddArtifact(ctx, a, locRef.Name()); err != nil {
+					t.Errorf("Failed to generate store contents for CopyAll(): %v", err)
+				}
+			}
+
+			if descs, err := s.CopyAll(tt.args.ctx, tt.args.to, tt.args.toMapper); (err != nil) != tt.wantErr {
+				t.Errorf("CopyAll() error = %v, wantErr %v", err, tt.wantErr)
+			} else if len(descs) != len(tt.args.refs) {
+				t.Errorf("CopyAll() expected to push %d descriptors, but only pushed %d", len(descs), len(tt.args.refs))
+			}
+		})
+	}
 }
