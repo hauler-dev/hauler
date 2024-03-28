35538bf45a61c7dae2bc47180195674fac8d2576
Adam Martin <adam.martin@rancherfederal.com>
2024-03-28 14:24:37 -0400

    removed misleading cache flag
    Signed-off-by: Adam Martin <adam.martin@rancherfederal.com>

--- cmd/hauler/cli/store/flags.go
+++ cmd/hauler/cli/store/flags.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/rancherfederal/hauler/pkg/layer"
 	"github.com/rancherfederal/hauler/pkg/store"
 	"github.com/spf13/cobra"
 
@@ -15,17 +14,14 @@ import (
 
 const (
 	DefaultStoreName = "store"
-	DefaultCacheDir  = "hauler"
 )
 
 type RootOpts struct {
 	StoreDir string
-	CacheDir string
 }
 
 func (o *RootOpts) AddArgs(cmd *cobra.Command) {
 	pf := cmd.PersistentFlags()
-	pf.StringVar(&o.CacheDir, "cache", "", "Location of where to store cache data (defaults to $XDG_CACHE_DIR/hauler)")
 	pf.StringVarP(&o.StoreDir, "store", "s", DefaultStoreName, "Location to create store at")
 }
 
@@ -48,37 +44,9 @@ func (o *RootOpts) Store(ctx context.Context) (*store.Layout, error) {
 		return nil, err
 	}
 
-	// TODO: Do we want this to be configurable?
-	c, err := o.Cache(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	s, err := store.NewLayout(abs, store.WithCache(c))
+	s, err := store.NewLayout(abs)
 	if err != nil {
 		return nil, err
 	}
 	return s, nil
-}
-
-func (o *RootOpts) Cache(ctx context.Context) (layer.Cache, error) {
-	dir := o.CacheDir
-
-	if dir == "" {
-		// Default to $XDG_CACHE_HOME
-		cachedir, err := os.UserCacheDir()
-		if err != nil {
-			return nil, err
-		}
-
-		abs, _ := filepath.Abs(filepath.Join(cachedir, DefaultCacheDir))
-		if err := os.MkdirAll(abs, os.ModePerm); err != nil {
-			return nil, err
-		}
-
-		dir = abs
-	}
-
-	c := layer.NewFilesystemCache(dir)
-	return c, nil
 }
