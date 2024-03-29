7083f3a4f30f903b6b426c475fb6e0b36a71cae3
Adam Martin <adam.martin@rancherfederal.com>
2024-03-29 13:40:24 -0400

    deprecate the cache flag instead of remove
    Signed-off-by: Adam Martin <adam.martin@rancherfederal.com>

--- cmd/hauler/cli/store/flags.go
+++ cmd/hauler/cli/store/flags.go
@@ -18,11 +18,13 @@ const (
 
 type RootOpts struct {
 	StoreDir string
+	CacheDir string
 }
 
 func (o *RootOpts) AddArgs(cmd *cobra.Command) {
 	pf := cmd.PersistentFlags()
 	pf.StringVarP(&o.StoreDir, "store", "s", DefaultStoreName, "Location to create store at")
+	pf.StringVar(&o.CacheDir, "cache", "", "(deprecated flag and currently not used)")
 }
 
 func (o *RootOpts) Store(ctx context.Context) (*store.Layout, error) {
