4bbe622073ca7b476d9f1b654970273d42aa51f0
Adam Martin <adam.martin@rancherfederal.com>
2024-04-01 09:39:34 -0400

    add extra info for the tempdir override flag
    Signed-off-by: Adam Martin <adam.martin@rancherfederal.com>

--- cmd/hauler/cli/store/load.go
+++ cmd/hauler/cli/store/load.go
@@ -20,11 +20,11 @@ type LoadOpts struct {
 func (o *LoadOpts) AddFlags(cmd *cobra.Command) {
 	f := cmd.Flags()
 
-	// On Unix systems, TempDir returns $TMPDIR if non-empty, else /tmp.
-	// On Windows, TempDir uses GetTempPath, returning the first non-empty
+	// On Unix systems, the default is $TMPDIR if non-empty, else /tmp.
+	// On Windows, the default is GetTempPath, returning the first non-empty
 	// value from %TMP%, %TEMP%, %USERPROFILE%, or the Windows directory.
-	// On Plan 9, TempDir returns /tmp.
-	f.StringVarP(&o.TempOverride, "temp", "t", "", "overrides the default directory for temporary files, as returned by your OS.")
+	// On Plan 9, the default is /tmp.
+	f.StringVarP(&o.TempOverride, "tempdir", "t", "", "overrides the default directory for temporary files, as returned by your OS.")
 }
 
 // LoadCmd
