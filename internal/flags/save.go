75305110b5d50de5e095602bf3f542fcd194c3ba
Zack Brady <zackbrady123@gmail.com>
2024-09-30 14:50:39 -0400

    fixed typo in hauler store save

--- internal/flags/save.go
+++ internal/flags/save.go
@@ -12,5 +12,5 @@ func (o *SaveOpts) AddFlags(cmd *cobra.Command) {
 	f := cmd.Flags()
 
 	f.StringVarP(&o.FileName, "filename", "f", "haul.tar.zst", "(Optional) Specify the name of outputted archive")
-	f.StringVarP(&o.Platform, "platform", "p", "", "(Optional) Specifiy the platform of the images for the outputted archive... i.e. linux/amd64 (defaults to all)")
+	f.StringVarP(&o.Platform, "platform", "p", "", "(Optional) Specify the platform for runtime imports... i.e. linux/amd64 (defaults to all)")
 }
