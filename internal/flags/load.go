382dea42a5a06da23fd5871872ac37806dabe3c6
Zack Brady <zackbrady123@gmail.com>
2026-01-07 08:31:40 -0500

    updated tempdir flag to store persistent flags (#484)

--- internal/flags/load.go
+++ internal/flags/load.go
@@ -7,15 +7,11 @@ import (
 
 type LoadOpts struct {
 	*StoreRootOpts
-	FileName     []string
-	TempOverride string
+	FileName []string
 }
 
 func (o *LoadOpts) AddFlags(cmd *cobra.Command) {
 	f := cmd.Flags()
 
-	// On Unix systems, the default is $TMPDIR if non-empty, else /tmp
-	// On Windows, the default is GetTempPath, returning the first value from %TMP%, %TEMP%, %USERPROFILE%, or Windows directory
 	f.StringSliceVarP(&o.FileName, "filename", "f", []string{consts.DefaultHaulerArchiveName}, "(Optional) Specify the name of inputted haul(s)")
-	f.StringVarP(&o.TempOverride, "tempdir", "t", "", "(Optional) Override the default temporary directiory determined by the OS")
 }
