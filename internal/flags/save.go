4a2b7b13a7a3e3dab926893c0be1a67dea6d8457
Zack Brady <zackbrady123@gmail.com>
2026-01-15 20:45:24 -0500

    fixed typos for containerd imports (#493)

--- internal/flags/save.go
+++ internal/flags/save.go
@@ -17,6 +17,6 @@ func (o *SaveOpts) AddFlags(cmd *cobra.Command) {
 
 	f.StringVarP(&o.FileName, "filename", "f", consts.DefaultHaulerArchiveName, "(Optional) Specify the name of outputted haul")
 	f.StringVarP(&o.Platform, "platform", "p", "", "(Optional) Specify the platform for runtime imports... i.e. linux/amd64 (unspecified implies all)")
-	cmd.Flags().BoolVar(&o.ContainerdCompatibility, "containerd", false, "(Optional) Enable import compatibility containerd... removes oci-layout from the haul")
+	f.BoolVar(&o.ContainerdCompatibility, "containerd", false, "(Optional) Enable import compatibility with containerd... removes oci-layout from the haul")
 
 }
