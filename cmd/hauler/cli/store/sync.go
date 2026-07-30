8dc864ca77c0729bf3f9036881958e3c6324b3e3
Eric Klatzer <eric@klatzer.at>
2026-07-30 20:59:11 +0200

    feat: extend charts resource by platform (#657)
    Signed-off-by: Eric Klatzer <eric@klatzer.at>
    Co-authored-by: Zack Brady <zackbrady123@gmail.com>

--- cmd/hauler/cli/store/sync.go
+++ cmd/hauler/cli/store/sync.go
@@ -501,6 +501,14 @@ func processContent(ctx context.Context, fi *os.File, o *flags.SyncOpts, s *stor
 						valuesFiles = append(valuesFiles, filepath.Join(filepath.Dir(fi.Name()), path))
 					}
 
+					platform := o.Platform
+					if annotation != nil && annotation[consts.ImageAnnotationPlatform] != "" {
+						platform = annotation[consts.ImageAnnotationPlatform]
+					}
+					if ch.Platform != "" {
+						platform = ch.Platform
+					}
+
 					if err := storeChart(ctx, s, ch,
 						&flags.AddChartOpts{
 							ChartOpts: &action.ChartPathOptions{
@@ -511,7 +519,7 @@ func processContent(ctx context.Context, fi *os.File, o *flags.SyncOpts, s *stor
 							AddDependencies: ch.AddDependencies,
 							ExcludeExtras:   excludeExtras,
 							Registry:        registry,
-							Platform:        o.Platform,
+							Platform:        platform,
 							ValuesFiles:     valuesFiles,
 						},
 						rso, ro,
