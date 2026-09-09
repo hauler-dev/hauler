b4558474c9fd5cd3a71c860737f64adeadaeda03
Zack Brady <zackbrady123@gmail.com>
2026-09-09 11:42:29 -0400

    added experimental flag to copy types

--- internal/flags/copy.go
+++ internal/flags/copy.go
@@ -21,7 +21,7 @@ func (o *CopyOpts) AddFlags(cmd *cobra.Command) {
 	f.BoolVar(&o.Insecure, "insecure", false, "(Optional) Allow insecure connections")
 	f.BoolVar(&o.PlainHTTP, "plain-http", false, "(Optional) Allow plain HTTP connections")
 	f.StringVarP(&o.Only, "only", "o", "", "(Optional) Custom string array to only copy specific 'image' items")
-	f.StringVar(&o.TypeFilter, "type", "all", "(Optional) Filter on content type (image | chart | file | sigs | atts | sbom | referrer)")
+	f.StringVar(&o.TypeFilter, "type", "all", "(EXPERIMENTAL) (Optional) Filter on content type (image | chart | file | sigs | atts | sbom | referrer)")
 
 	cmd.MarkFlagsRequiredTogether("username", "password")
 
