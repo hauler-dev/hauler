c61e95aa18926529fab2c770fc4e976919720dcd
Zack Brady <zackbrady123@gmail.com>
2026-09-23 18:56:43 -0400

    fixed adding local charts without a repo

--- pkg/artifacts/chart/chart_test.go
+++ pkg/artifacts/chart/chart_test.go
@@ -146,3 +146,13 @@ func TestNewChart_VerifyOnUnsignedChartFails(t *testing.T) {
 		t.Fatalf("expected provenance error, got: %v", err)
 	}
 }
+
+// a local chart path added without --repo is used as is, not turned into an absolute "/<path>".
+func TestNewChart_LocalPathWithoutRepo(t *testing.T) {
+	t.Chdir("../../../testdata")
+	for _, name := range []string{"rancher-cluster-templates-0.5.2.tgz", "./rancher-cluster-templates-0.5.2.tgz"} {
+		if _, err := chart.NewChart(name, &action.ChartPathOptions{}); err != nil {
+			t.Errorf("NewChart(%q) without a repo: %v", name, err)
+		}
+	}
+}
